939e3319bc122899592f0eae8fdf00da4c15d9b9
iksanh <iksanhariji@gmail.com>
2023-06-13 10:21:29 +0800

    created checkout page along with checkout item and check out cart functionality

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,48 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+import CheckOutItem from "../../components/checkout-item/checkout-item.component";
+import "./checkout.styles.scss";
+
+const CheckOut = () => {
+  const { cartItems, cartTotal } = useContext(CartContext);
+
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map(
+        (cartItem) => (
+          <CheckOutItem key={cartItem.id} cartItem={cartItem} />
+        )
+        // <div key={id}>
+        //   <h1>{name}</h1>
+        //   <span>{quantity}</span>
+        //   <br />
+        //   <span onClick={incrementItemCart.bind(this, cartitem)}>
+        //     increment
+        //   </span>
+        //   <br />
+        //   <span onClick={() => removeItemToCart(cartitem)}>decrement</span>
+        // </div>
+      )}
+      <span className="total">Total : ${cartTotal}</span>
+    </div>
+  );
+};
+
+export default CheckOut;
